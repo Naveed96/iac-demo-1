52bfea320eac446f9458e76ee03362348353359d
Naveed96 <31357004+Naveed96@users.noreply.github.com>
2021-04-23 17:43:24 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -3,12 +3,3 @@ provider "aws" {
   region     = "ap-south-1"
 }
 
-resource "aws_instance" "naveed_server" {
-  ami           = "ami-0b84c6433cdbe5c3e"
-  instance_type = "t2.micro"
-
-  tags = {
-     Name = "Sample Application"
- }
-
-}
